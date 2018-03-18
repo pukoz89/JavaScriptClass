1d7a4a7cfa92c0f0ca9be625407502e5b68fe534
Christian <christian_zapata@hotmail.com>
2018-03-17 21:08:17 -0500

    Writing my first conditional

---
+++ condicional.js
@@ -0,0 +1,35 @@
+	var members = 3
+	if (members <= 0) {
+		console.log("not a group");
+	}
+	else if (members == 1) {
+		console.log ("solo");
+	} 
+	else if (members == 2) {
+		console.log ("duet");
+	} 
+	else if (members == 3) {
+		console.log ("trio");
+	} 
+	else if (members == 4) {
+		console.log ("quartet");
+	}
+	else console.log ("this is a large group");
+
+		var members = 3
+	if (members <= 0) {
+		console.log("not a group");
+	}
+	else if (members = 1) {
+		console.log ("solo");
+	} 
+	else if (members = 2) {
+		console.log ("duet");
+	} 
+	else if (members = 3) {
+		console.log ("trio");
+	} 
+	else if (members = 4) {
+		console.log ("quartet");
+	}
+	else console.log ("this is a large group");
